d3410f4c054973f859c047e43d632fb3863eb23a
Karol Chilimoniuk <webdevkarolchilimoniuk@gmail.com>
2023-03-12 15:01:15 +0100

    feat: Another method of deployment - only backend

--- index.ts
+++ index.ts
@@ -3,7 +3,6 @@ import mongoose from "mongoose";
 import dotenv from "dotenv";
 import bodyParser from "body-parser";
 import cookieParser from "cookie-parser";
-import * as path from "path";
 import mainRouter from "./routes/main";
 import authRouter from "./routes/auth";
 import userActivitiesRouter from "./routes/userActivities";
@@ -45,14 +44,6 @@ app.use(cookieParser());
 app.use("/", mainRouter);
 app.use("/auth", authRouter);
 app.use("/logged", userActivitiesRouter);
-
-// ----- static folder for "production" -----
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static("client/build"));
-  app.get("*", (req, res) =>
-    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"))
-  );
-}
 
 main()
   .then(() => {
